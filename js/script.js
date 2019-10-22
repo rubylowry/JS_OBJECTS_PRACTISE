cdd442bcdaaa1915c7a97df024cf5c03f7b58398
Ruby Lowry <270010549@bun2-206-15.local>
2019-10-22 15:19:06 +1300

    adding variables in js

---
+++ js/script.js
@@ -0,0 +1,77 @@
+console.log("15 Objects");
+
+// The properties of Objects
+
+var cheese = [
+  {
+    name: "Blue Cheese",
+    id: "blueCheese",
+    price: 12,
+    country: "France",
+    lactose: true,
+    vegan: false,
+    vegetarian: true,
+    photo: "img/bc.jpg"
+  },
+  {
+    name: "Mozzarella",
+    id: "mozzarella",
+    price: 8,
+    country: "Italy",
+    lactose: true,
+    vegan: false,
+    vegetarian: true,
+    photo: "img/mozzarella.jpg"
+  },
+  {
+    name: "Cheddar",
+    id: "cheddar",
+    price: 10,
+    country: "England",
+    lactose: true,
+    vegan: false,
+    vegetarian: true,
+    photo: "img/cheddar.jpg"
+  },
+  {
+    name: "Emmental",
+    id: "emmental",
+    price: 9,
+    country: "Switzerland",
+    lactose: true,
+    vegan: false,
+    vegetarian: true,
+    photo: "img/emmental.jpg"
+  },
+  {
+    name: "Gouda",
+    id: "gouda",
+    price: 9,
+    country: "Switzerland",
+    lactose: true,
+    vegan: false,
+    vegetarian: true,
+    photo: "img/gouda.jpg"
+  },
+  {
+    name: "Brie",
+    id: "brie",
+    price: 14,
+    country: "France",
+    lactose: true,
+    vegan: false,
+    vegetarian: true,
+    photo: "img/brie.jpg"
+  },
+  {
+    name: "Mascarpone",
+    id: "mascarpone",
+    price: 14,
+    country: "Italy",
+    lactose: true,
+    vegan: false,
+    vegetarian: true,
+    photo: "img/mascarpone.jpg"
+  },
+
+]
